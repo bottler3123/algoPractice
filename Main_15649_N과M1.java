70eb499b9ece0a24177f89de3d5ac9d45c2db3b6
bottler3123 <109344835+bottler3123@users.noreply.github.com>
2022-10-25 15:40:03 +0900

    Add files via upload

---
+++ Main_15649_N과M1.java
@@ -0,0 +1,38 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Main_15649_N과M1 {
+
+	static int N,M;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		int [] arr = new int [M];
+		boolean [] check = new boolean [N];
+		
+		dfs(arr,check,0);
+	}
+
+	private static void dfs(int[] arr, boolean[] check, int dep) {
+		if (dep == M) {
+			for (int ans : arr) {
+				System.out.print(ans + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			if (!check[i]) {
+				arr[dep] = i+1;
+				check[i] = true;
+				dfs(arr, check, dep+1);
+				check[i] = false;
+			}
+		}
+	}
+}

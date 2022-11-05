d9dab16bd3f1474a7587d9ac2a5f1aeb6c85aa52
bottler3123 <109344835+bottler3123@users.noreply.github.com>
2022-11-05 19:06:52 +0900

    Add files via upload

---
+++ Main_2744_대소문자바꾸기.java
@@ -0,0 +1,25 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Main_2744_대소문자바꾸기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		for (int i = 0; i < str.length(); i++) {
+			char change = str.charAt(i);
+			if (change < 97) {
+				change += 32;
+				System.out.print(change);
+			} else {
+				change -= 32;
+				System.out.print(change);				
+			}
+//			String aA = "aA";
+//			int a = aA.charAt(0);
+//			int A = aA.charAt(1);
+//			System.out.println(a + " " + A);
+		}
+	}
+}

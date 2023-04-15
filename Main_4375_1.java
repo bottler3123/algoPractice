d70b14f57b6a3c233ab7f8d4e488b8faba6f098f
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-04-16 02:38:20 +0900

    Add files via upload

---
+++ Main_4375_1.java
@@ -0,0 +1,27 @@
+package algo0408;
+
+import java.util.*;
+
+public class Main_4375_1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+		    int n = sc.nextInt();
+            int num = 0;
+            int cnt = 1;
+            while(true) {
+                num = (num * 10) + 1;
+                num = num % n;
+                if(num == 0) {
+                    System.out.println(cnt);
+                    break;
+                }
+                cnt++;
+            }
+		}
+		sc.close();
+	}
+	
+}

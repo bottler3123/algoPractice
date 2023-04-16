b9713b12c505582a46f24bde7487b667816f64e8
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-04-17 01:02:48 +0900

    Add files via upload

---
+++ Main_17427_약수의합2.java
@@ -0,0 +1,19 @@
+package algo0408;
+
+import java.io.*;
+
+public class Main_17427_약수의합2 {
+	
+	public static void main(String [] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		long result = 0;
+		for (int i = 1; i <= N; i++) {
+			result += N / i * i;
+		}
+		
+		System.out.println(result);
+	}
+
+}

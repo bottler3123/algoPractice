ea25d180732e7a121016053538de16d5b1b6c8cd
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-04-27 01:35:36 +0900

    Add files via upload

---
+++ boj_Bronze/Main_10926_qqe.java
@@ -0,0 +1,14 @@
+package algo0408;
+
+import java.util.Scanner;
+
+public class Main_10926_qqe {
+	// boj Bronze5
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		System.out.println(str+"??!");
+	}
+}

83d2a1c0e4166fff41e56e638155a277d48988ea
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-04-30 23:52:05 +0900

    Add files via upload

---
+++ boj_Bronze/Main_18108_1998년생인내가태국에서는2541년생.java
@@ -0,0 +1,14 @@
+package algo0408;
+
+import java.util.Scanner;
+
+public class Main_18108_1998년생인내가태국에서는2541년생 {
+	// boj Bronze5
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int y = sc.nextInt();
+		System.out.println(y - 543);
+	}
+
+}

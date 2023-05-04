5ff671c7977e37c9e342af73fdfe37d37448b033
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-05-05 05:14:04 +0900

    Add files via upload

---
+++ boj_Bronze/Main_2743_단어길이재기.java
@@ -0,0 +1,13 @@
+package algo0408;
+
+import java.util.*;
+
+public class Main_2743_단어길이재기 {
+	// boj Bronze5
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(str.length());
+	}
+}

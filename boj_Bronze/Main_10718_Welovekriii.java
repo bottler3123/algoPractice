f753a0708ae0232a5d9e33bae454c8eceb32c29e
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-05-12 00:11:05 +0900

    Add files via upload

---
+++ boj_Bronze/Main_10718_Welovekriii.java
@@ -0,0 +1,10 @@
+package algo0408;
+
+public class Main_10718_Welovekriii {
+	// boj Bronze5
+	
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}

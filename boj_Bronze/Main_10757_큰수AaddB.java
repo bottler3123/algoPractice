6ea89887f2fec362b27f4a68c08f4a908f9fcc45
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-05-14 23:39:10 +0900

    Add files via upload

---
+++ boj_Bronze/Main_10757_큰수AaddB.java
@@ -0,0 +1,17 @@
+package algo0408;
+
+import java.math.*;
+import java.util.*;
+
+public class Main_10757_큰수AaddB {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger n = sc.nextBigInteger();
+		BigInteger m = sc.nextBigInteger();
+		
+		System.out.println(n.add(m));
+		System.out.println(n.subtract(m));
+		System.out.println(n.multiply(m));
+	}
+}

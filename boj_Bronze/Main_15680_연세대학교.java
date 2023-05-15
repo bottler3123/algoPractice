35d7a39b7ef9f4de99b058368cc0deaa132c8390
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-05-15 20:23:35 +0900

    Add files via upload

---
+++ boj_Bronze/Main_15680_연세대학교.java
@@ -0,0 +1,19 @@
+package algo0408;
+
+import java.util.*;
+
+public class Main_15680_연세대학교 {
+	// boj Bronze5
+	
+	public static void main (String [] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		if (N == 0) {
+			System.out.println("YONSEI");
+		} else {
+			System.out.println("Leading the Way to the Future");
+		}
+	}
+}

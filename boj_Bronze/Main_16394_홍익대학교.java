076e18b6f473d350c7d3a5ee9ab0da8188b65e15
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-05-20 23:44:03 +0900

    Add files via upload

---
+++ boj_Bronze/Main_16394_홍익대학교.java
@@ -0,0 +1,14 @@
+package algo0408;
+
+import java.util.Scanner;
+
+public class Main_16394_홍익대학교 {
+	// boj Bronze5
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		System.out.println(N - 1946);
+	}
+}

245c8d32928625727665aafa324c40c997783111
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-06-10 22:42:10 +0900

    Add files via upload

---
+++ boj_Bronze/Main_2443_별찍기6.java
@@ -0,0 +1,21 @@
+package algo0408;
+
+import java.util.*;
+
+public class Main_2443_별찍기6 {
+	// boj Bronze3
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = N; i > 0; i--) {
+			for (int j = 0; j < N - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = i*2-1; j > 0 ; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}

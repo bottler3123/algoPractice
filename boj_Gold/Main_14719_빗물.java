057a07a0c134365f5cbf18b2f402b4295cba596e
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-06-18 00:41:11 +0900

    Add files via upload

--- boj_Gold/Main_14719_빗물.java
+++ boj_Gold/Main_14719_빗물.java
@@ -24,12 +24,12 @@ public class Main_14719_빗물 {
 		int result = 0;
 		
 		// map 표시
-		for (int i = 0; i < H; i++) {
-			for (int j = 0; j < W; j++) {
-				System.out.print(map[i][j] + " ");
-			}
-			System.out.println();
-		}
+//		for (int i = 0; i < H; i++) {
+//			for (int j = 0; j < W; j++) {
+//				System.out.print(map[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
 		
 		for (int i = 0; i < H; i++) {
 			for (int j = 0; j < W -1; j++) {

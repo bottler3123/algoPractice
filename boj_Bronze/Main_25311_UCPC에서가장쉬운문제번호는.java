a89cedf0adb7b1182627b06b3d43ce8bd8bea133
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-06-22 13:05:57 +0900

    Add files via upload

---
+++ boj_Bronze/Main_25311_UCPC에서가장쉬운문제번호는.java
@@ -0,0 +1,14 @@
+package algo0408;
+
+import java.util.*;
+
+public class Main_25311_UCPC에서가장쉬운문제번호는 {
+	// boj Bronze5
+	
+			public static void main(String[] args) {
+				Scanner sc = new Scanner(System.in);
+				int y = sc.nextInt();
+
+				System.out.println("A");
+			}
+}

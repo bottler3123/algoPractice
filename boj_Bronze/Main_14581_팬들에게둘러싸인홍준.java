4c8cee44c0005194e69602c8dfbe40634929722e
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-06-26 03:20:13 +0900

    Add files via upload

---
+++ boj_Bronze/Main_14581_팬들에게둘러싸인홍준.java
@@ -0,0 +1,16 @@
+package algo0408;
+
+import java.util.Scanner;
+
+public class Main_14581_팬들에게둘러싸인홍준 {
+	// boj Bronze5
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::" + name + "::fan:");
+		System.out.println(":fan::fan::fan:");
+	}
+}

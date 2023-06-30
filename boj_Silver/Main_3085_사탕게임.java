664a81672f6adfcedb5eaff7aaf3599abfd9a764
pbw <bottler3123@naver.com>
2023-06-30 18:31:02 +0900

    sliver2 2085 사탕게임 해결

--- boj_Silver/Main_3085_사탕게임.java
+++ boj_Silver/Main_3085_사탕게임.java
@@ -1,4 +1,4 @@
-package algo0408;
+package algo;
 
 import java.util.*;
 import java.io.*;
@@ -100,6 +100,68 @@ public class Main_3085_사탕게임 {
 					}
 				}
 			}
+			for (int i = 0; i < N; i++) {
+				int color [] = new int [4];
+				int temp = 1;
+				for (int j = 0; j < N; j++) {
+					if (j == N-1) {
+						switch (board[i][j]) {
+						case 'C':
+							if (color[0] < temp) {
+								color[0] = temp;
+							}
+							break;
+						case 'P':
+							if (color[1] < temp) {
+								color[1] = temp;
+							}
+							break;
+						case 'Z':
+							if (color[2] < temp) {
+								color[2] = temp;
+							}
+							break;
+						case 'Y':
+							if (color[3] < temp) {
+								color[3] = temp;
+							}
+							break;
+						}
+					} else if (board[i][j] == board[i][j+1]) {
+						temp++;
+					} else {
+						switch (board[i][j]) {
+						case 'C':
+							if (color[0] < temp) {
+								color[0] = temp;
+							}
+							break;
+						case 'P':
+							if (color[1] < temp) {
+								color[1] = temp;
+							}
+							break;
+						case 'Z':
+							if (color[2] < temp) {
+								color[2] = temp;
+							}
+							break;
+						case 'Y':
+							if (color[3] < temp) {
+								color[3] = temp;
+							}
+							break;
+						}
+						temp = 1;
+					}
+				}
+				for (int j = 0; j < 4; j++) {
+//					System.out.println(j+"번 캔디를 "+color[j]+" 먹음");
+					if (result < color[j]) {
+						result = color[j];
+					}
+				}
+			}
 			
 			board[row][col+1] = board[row][col];
 			board[row][col] = candy;
@@ -112,6 +174,68 @@ public class Main_3085_사탕게임 {
 			board[row][col] = board[row+1][col];
 			board[row+1][col] = candy;
 			
+			for (int i = 0; i < N; i++) {
+				int color [] = new int [4];
+				int temp = 1;
+				for (int j = 0; j < N; j++) {
+					if (j == N-1) {
+						switch (board[j][i]) {
+						case 'C':
+							if (color[0] < temp) {
+								color[0] = temp;
+							}
+							break;
+						case 'P':
+							if (color[1] < temp) {
+								color[1] = temp;
+							}
+							break;
+						case 'Z':
+							if (color[2] < temp) {
+								color[2] = temp;
+							}
+							break;
+						case 'Y':
+							if (color[3] < temp) {
+								color[3] = temp;
+							}
+							break;
+						}
+					} else if (board[j][i] == board[j+1][i]) {
+						temp++;
+					} else {
+						switch (board[j][i]) {
+						case 'C':
+							if (color[0] < temp) {
+								color[0] = temp;
+							}
+							break;
+						case 'P':
+							if (color[1] < temp) {
+								color[1] = temp;
+							}
+							break;
+						case 'Z':
+							if (color[2] < temp) {
+								color[2] = temp;
+							}
+							break;
+						case 'Y':
+							if (color[3] < temp) {
+								color[3] = temp;
+							}
+							break;
+						}
+						temp = 1;
+					}
+				}
+				for (int j = 0; j < 4; j++) {
+	//				System.out.println(j+"번 캔디를 "+color[j]+" 먹음");
+					if (result < color[j]) {
+						result = color[j];
+					}
+				}
+			}
 			for (int i = 0; i < N; i++) {
 				int color [] = new int [4];
 				int temp = 1;

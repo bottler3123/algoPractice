6797a6c62654134fbe83d9039038c9b03c43d19c
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-07-14 23:49:21 +0900

    Add files via upload

---
+++ boj_Bronze/Main_2393_Rook.java
@@ -0,0 +1,21 @@
+package algo0408;
+
+public class Main_2393_Rook {
+	// boj Bronze5
+	
+	public static void main(String[] args) {
+        System.out.println("  ___  ___  ___");
+        System.out.println("  | |__| |__| |");
+        System.out.println("  |           |");
+        System.out.println("   \\_________/");
+        System.out.println("    \\_______/");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |_____|");
+        System.out.println("  __/       \\__");
+        System.out.println(" /             \\");
+        System.out.println("/_______________\\");
+	}
+}

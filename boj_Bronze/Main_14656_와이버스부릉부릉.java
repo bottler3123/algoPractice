25d32700beedf9cc5beafa20057154d29da8bd58
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-07-22 23:55:01 +0900

    Add files via upload

---
+++ boj_Bronze/Main_14656_와이버스부릉부릉.java
@@ -0,0 +1,19 @@
+package algo0408;
+
+import java.util.Scanner;
+
+public class Main_14656_와이버스부릉부릉 {
+	// boj Bronze5
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+		}
+		System.out.println("비와이");
+	}
+
+}

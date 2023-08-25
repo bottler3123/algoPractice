5e428ee2f22b35e2fd2bdf71d42f307cb468c24d
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-08-25 23:55:52 +0900

    Add files via upload

--- boj_Silver/Main_24511_queuestack.java
+++ boj_Silver/Main_24511_queuestack.java
@@ -5,10 +5,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_24511_queuestack {

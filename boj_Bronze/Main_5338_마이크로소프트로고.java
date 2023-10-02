0b897299c883c126d08a804078c5850479f85f0e
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-10-02 22:53:17 +0900

    Add files via upload

---
+++ boj_Bronze/Main_5338_마이크로소프트로고.java
@@ -0,0 +1,13 @@
+package algo0408;
+
+public class Main_5338_마이크로소프트로고 {
+	// boj Bronze5
+	
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._\n" + 
+				"'-..-'|   ||   |\n" + 
+				"'-..-'|_.-;;-._|\n" + 
+				"'-..-'|   ||   |\n" + 
+				"'-..-'|_.-''-._|");
+	}
+}

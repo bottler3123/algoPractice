512eac8dc7b912f83a4e4a8d6383ca2a80e8b5c0
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-10-25 16:03:53 +0900

    Add files via upload

---
+++ boj_Silver/Main_11403_경로찾기.java
@@ -0,0 +1,45 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main_11403_경로찾기 {
+	// boj Silver1
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		int[][] map = new int [N][N];
+		
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < N; k++) {
+					if (map[j][i] != 0 && map[i][k] != 0) {
+						map[j][k] = 1;
+					}
+				}
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				bw.append(map[i][j] + " ");
+			}
+			bw.append("\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+}

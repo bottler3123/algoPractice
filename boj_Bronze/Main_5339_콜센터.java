061885e6a0ab8eb559d7eee81f1a70378f80121e
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-11-09 02:28:41 +0900

    Add files via upload

---
+++ boj_Bronze/Main_5339_콜센터.java
@@ -0,0 +1,18 @@
+package algo0408;
+
+public class Main_5339_콜센터 {
+	// boj Bronze5
+	
+	public static void main(String[] args) {
+		System.out.println("     /~\\\n" + 
+				"    ( oo|\n" + 
+				"    _\\=/_\n" + 
+				"   /  _  \\\n" + 
+				"  //|/.\\|\\\\\n" + 
+				" ||  \\ /  ||\n" + 
+				"============\n" + 
+				"|          |\n" + 
+				"|          |\n" + 
+				"|          |");
+	}
+}

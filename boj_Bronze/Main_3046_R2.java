1855c5e4e8662681da876dd78f48e1371a9f115a
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-11-19 00:37:48 +0900

    Add files via upload

---
+++ boj_Bronze/Main_3046_R2.java
@@ -0,0 +1,17 @@
+package algo0408;
+
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+public class Main_3046_R2 {
+	// boj Bronze4
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int R1 = sc.nextInt();
+		int S = sc.nextInt();
+		int result = S * 2 - R1;
+		
+		System.out.println(result);
+	}
+}

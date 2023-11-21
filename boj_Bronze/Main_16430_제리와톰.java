1948af92d0ed25e60263af3622464964519d6cc6
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-11-22 01:39:04 +0900

    Add files via upload

---
+++ boj_Bronze/Main_16430_제리와톰.java
@@ -0,0 +1,15 @@
+package algo0408;
+
+import java.util.Scanner;
+
+public class Main_16430_제리와톰 {
+	// boj Bronze5
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(B-A + " " + B);
+	}
+}

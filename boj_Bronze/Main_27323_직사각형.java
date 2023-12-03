d478b587f1687c89e927040c83ee80a448b68497
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-12-03 23:19:20 +0900

    Add files via upload

---
+++ boj_Bronze/Main_27323_직사각형.java
@@ -0,0 +1,15 @@
+package algo0408;
+
+import java.util.Scanner;
+
+public class Main_27323_직사각형 {
+	// boj Bronze5
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a * b);
+	}
+}

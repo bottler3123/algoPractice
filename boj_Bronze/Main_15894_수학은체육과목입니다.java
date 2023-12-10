020fad7ca422cdc7f3064588e38258f49e560547
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-12-10 17:53:03 +0900

    Add files via upload

---
+++ boj_Bronze/Main_15894_수학은체육과목입니다.java
@@ -0,0 +1,14 @@
+package algo0408;
+
+import java.util.Scanner;
+
+public class Main_15894_수학은체육과목입니다 {
+	// boj Bronze3
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextInt();
+		
+		System.out.println(4 * n);
+	}
+}

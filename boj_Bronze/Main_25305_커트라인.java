85162d4485135b80ee31ad03b403ff3411b52c8a
bang_woo <109344835+bottler3123@users.noreply.github.com>
2023-12-13 22:50:31 +0900

    Add files via upload

---
+++ boj_Bronze/Main_25305_커트라인.java
@@ -0,0 +1,22 @@
+package algo0408;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_25305_커트라인 {
+	// boj Bronze2
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int k = sc.nextInt();
+		int[] arr = new int [N];
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		System.out.println(arr[N-k]);
+	}
+}

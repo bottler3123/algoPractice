d04f55c84e997ac860e9f3f706b6cb7749380e86
bang_woo <109344835+bottler3123@users.noreply.github.com>
2024-01-06 16:20:32 +0900

    Add files via upload

---
+++ boj_Bronze/Main_15439_베라의패션.java
@@ -0,0 +1,13 @@
+package algo0408;
+
+import java.util.Scanner;
+
+public class Main_15439_베라의패션 {
+	// boj Bronze4
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N * (N-1));
+	}
+}

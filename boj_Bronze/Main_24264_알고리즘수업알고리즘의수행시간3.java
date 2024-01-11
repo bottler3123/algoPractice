9a82ee934b18144fe1dda8f4d6939d03808554c4
bang_woo <109344835+bottler3123@users.noreply.github.com>
2024-01-11 20:13:05 +0900

    Add files via upload

---
+++ boj_Bronze/Main_24264_알고리즘수업알고리즘의수행시간3.java
@@ -0,0 +1,15 @@
+	package algo;
+	
+	import java.util.Scanner;
+	
+	public class Main_24264_알고리즘수업알고리즘의수행시간3 {
+		// boj Bronze3
+		
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			Long n = sc.nextLong();
+			
+			System.out.println(n * n);
+			System.out.println(2);
+		}
+	}

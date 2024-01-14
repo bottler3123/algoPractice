5b5c2c53b69e68e07bebb26a03bad6258fa8459b
bang_woo <109344835+bottler3123@users.noreply.github.com>
2024-01-14 21:25:18 +0900

    Add files via upload

---
+++ boj_Silver/Main_24313_알고리즘수업점근적표기1.java
@@ -0,0 +1,22 @@
+package algo;
+
+import java.util.Scanner;
+
+public class Main_24313_알고리즘수업점근적표기1 {
+	// boj Silver5
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a1 = sc.nextInt();
+		int a0 = sc.nextInt();
+		int c = sc.nextInt();
+		int n = sc.nextInt();
+		
+		boolean check = a1 * n + a0 <= c * n ? true : false;
+		if (check) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
+		}
+	}
+}

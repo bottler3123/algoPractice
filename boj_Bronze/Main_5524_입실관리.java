a5a85425595e99f89b23ca8db94f96af59eee50b
bang_woo <109344835+bottler3123@users.noreply.github.com>
2024-01-20 00:54:44 +0900

    Add files via upload

---
+++ boj_Bronze/Main_5524_입실관리.java
@@ -0,0 +1,17 @@
+package algo;
+
+import java.util.Scanner;
+
+public class Main_5524_입실관리 {
+	// boj Bronze4
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for (int i = 0; i < N; i++) {
+			String str = sc.next();
+			System.out.println(str.toLowerCase());
+		}
+	}
+}

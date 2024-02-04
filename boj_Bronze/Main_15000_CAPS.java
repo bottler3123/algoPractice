f8c88f1ebd05050240fd0aa5becb557f85973832
bang_woo <109344835+bottler3123@users.noreply.github.com>
2024-02-04 18:12:13 +0900

    Add files via upload

---
+++ boj_Bronze/Main_15000_CAPS.java
@@ -0,0 +1,14 @@
+package algo;
+
+import java.util.Scanner;
+
+public class Main_15000_CAPS {
+	// boj Bronze5
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		System.out.println(str.toUpperCase());
+	}
+}

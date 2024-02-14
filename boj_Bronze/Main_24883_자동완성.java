0879d3aacc7f20aa91fd79536d1c11d5971bf910
bang_woo <109344835+bottler3123@users.noreply.github.com>
2024-02-14 23:49:29 +0900

    Add files via upload

---
+++ boj_Bronze/Main_24883_자동완성.java
@@ -0,0 +1,17 @@
+package algo;
+
+import java.util.Scanner;
+
+public class Main_24883_자동완성 {
+	// boj Bronze5
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		if (str.equals("n") || str.equals("N")) {
+			System.out.println("Naver D2");
+		} else {
+			System.out.println("Naver Whale");
+		}
+	}
+}

565d95d1a88b9ca9ead408023135f64388ca3516
bang_woo <109344835+bottler3123@users.noreply.github.com>
2024-02-16 19:10:40 +0900

    Add files via upload

---
+++ boj_Bronze/Main_1703_생장점.java
@@ -0,0 +1,29 @@
+package algo;
+
+import java.util.Scanner;
+
+public class Main_1703_생장점 {
+	// boj Bronze3;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			int n = sc.nextInt();
+			
+			if (n == 0) {
+				break;
+			}
+			
+			int leef = 1;
+			for (int i = 0; i < n; i++) {
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+				
+				leef = leef * a - b;
+			}
+			
+			System.out.println(leef);
+			sc.nextLine();
+		}
+	}
+}
